00c3d896e78b12a119db8f0a03b4482b74933f59
Iulia Iordanescu <39636748+iulia-iordanescu@users.noreply.github.com>
2018-06-03 19:59:22 -0400

    end of the day polish

---
+++ Java/Hello_World_Java_Coding.java
@@ -0,0 +1,10 @@
+public class Hello_World_Java_Coding {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+      double x= 3;
+      System.out.printf("sqrt of %.0f is %.11f%n", x, Math.sqrt(x));
+   }
+}
+
+
